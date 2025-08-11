d255aa2836da7c136e76bc6fc475ab1cda3c2e86
Cuerdo, Clea Jana <cleajanacuerdo@gmail.com>
2025-08-11 23:22:28 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,5 @@
+#print("Xia Wu Hao, \n\nBSIT!")
+
+print("The Quick Brown \fJumps Over the Lazy\b\b\b Dog.!")
+
+print("\"Si Yiran lang and 99+ others\"")
---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name= input("Can you write your name?")
+
+print("\t\t\t\t\t*\t\t\t\t\n\n\t\t\t\t*\t\t*\n\n\t\t\t*\tHi,\t\t\t*\t\t\t\t\n\n\t\t*\t\t\t\t\t\t*\t\t\t\t\n\n\t*\t\t\t\t",name,"\t\t\t\t*\t\t\t\t\t\n\n\n\t\t*\t\t\t\t\t\t*\t\t\t\t\n\n\t\t\t*\t\t\t\t*\t\t\t\t\n\n\t\t\t\t*\t\t*\t\t\n\n\t\t\t\t\t*\t\t\t\t\t")
