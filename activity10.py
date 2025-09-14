23b3f4e3e3949c39054a4294cecc1fee1ffc5a71
Cuerdo, Clea Jana <cleajanacuerdo@gmail.com>
2025-09-14 18:58:14 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "deluluako.com"
+password = "ZedLoid_143"
+
+uname = input("Enter your username ---> ")
+pword = input("Enter your password ---> ")
+
+if (uname == username) and (pword == password):
+	print("Z is for C")
+else:
+	print("z is not for C")
