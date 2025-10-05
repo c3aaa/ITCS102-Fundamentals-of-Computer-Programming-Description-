f74af95ef4e3a6121fa461d89d25c33db34dd585
Cuerdo, Clea Jana <cleajanacuerdo@gmail.com>
2025-10-05 13:45:18 +0800

    Create activity20.py

---
+++ activity20.py
@@ -0,0 +1,16 @@
+for i in range(1, 11, 1):
+    for x in range(11, i, -1):
+        print(" ", end=" ")
+    for y in range(1, i-1, 1):
+        print("*", end=" ")
+    for c in range(1, i, 1):
+        print("*", end=" ")
+    print(" ")
+for i in range(1, 11, 1):
+    for x in range(1, i, 1):
+        print(" ", end=" ")
+    for y in range(11, i, -1):
+        print("*", end=" ")
+    for c in range(i, 10, 1):
+        print("*", end=" ")
+    print(" ")
